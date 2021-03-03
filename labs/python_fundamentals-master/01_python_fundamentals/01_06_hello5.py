6eaef2f8ecb5fef69a6473dc477be0eea9dbca3b
Ruben Vaz <ruben.vaz@gmail.com>
2021-03-03 22:53:33 +0000

    commit of the last exercices of 01_python_fundamentals

--- labs/python_fundamentals-master/01_python_fundamentals/01_06_hello5.py
+++ labs/python_fundamentals-master/01_python_fundamentals/01_06_hello5.py
@@ -4,3 +4,4 @@ Write the necessary code to display "Hello World!" 5 times.
 
 
 '''
+print("Hello World! " * 5)  # I've google the * 5 to do this in one line, or i've repeat the line of code 5 times!
--- labs/python_fundamentals-master/01_python_fundamentals/01_07_area_perimeter.py
+++ labs/python_fundamentals-master/01_python_fundamentals/01_07_area_perimeter.py
@@ -3,3 +3,9 @@
 Write the necessary code to display the area and perimeter of a rectangle that has a width of 2.4 and a height of 6.4.
 
 '''
+# Created 2 variables to show the results
+
+area = (2.4 * 6.4)
+print(area)
+perimeter = ((2*2.4)+(2*6.4))
+print(perimeter)
